4c7c65a32ae7e9524bf8094f063e935560f8cbf9
DanyHunter <dannytenaglias@gmail.com>
2013-04-23 19:20:32 -0500

    track files

---
+++ README.md
@@ -0,0 +1,6 @@
+== The Movie Playlist.
+
+1. Use Rotten Tomattoes API to make a better looking rottentomatoes.com
+2. add ability to create playlist, watched and unwatched movies. Find them, buy them, rent them, etc.
+3. Share with your friends.
+4. uh?
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoviePlaylist::Application.config.secret_token = '2f41cde413fe57c3f1d1d1ff3827f04c0396035f623b284075349b3490de1aba26b73ffa67bff419067005530c0fad1e5c2a193ca494265f0cb55cfe70a40378'
